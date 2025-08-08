97008f2c7e869d1575b340ae6c11249dd8357d30
M Charity <mcharity6498@gmail.com>
2025-08-08 15:59:01 -0400

    create word graph

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # amorphous-fortress-narratives
 Amorphous Fortress for Narrative Generation (EXAG 2025)
+
+
+Noun list:
+- http://supplies.thesmartteacher.com.s3.amazonaws.com/assets/exchange/list-of-nouns.pdf
+- https://gist.githubusercontent.com/creikey/42d23d1eec6d764e8a1d9fe7e56915c6/raw/b07de0068850166378bc3b008f9b655ef169d354/top-1000-nouns.txt
+- https://www.desiquintans.com/downloads/nounlist/nounlist.txt
--- grammar_experiments.py
+++ grammar_experiments.py
@@ -1,4 +1,6 @@
-from nltk.corpus import wordnet as wn
+#from nltk.corpus import wordnet as wn
+import spacy
+import requests
 
 # key: Key: E -- entity (symbol.id),  N -- same entity (symbol.new id), O -- other entity, P - position (x,y), Q -- other position
 log_set = [
@@ -13,5 +15,45 @@ log_set = [
         "[E_] blocked by wall [O_]"
     ]
 
-print(wn.synsets('moved', pos=wn.VERB))
 
+
+#print(wn.synsets('moved', pos=wn.VERB))
+
+
+# set up spacy with concepcy
+nlp = spacy.load("en_core_web_sm")
+noun_rel = ["RelatedTo", "PartOf", "IsA", "HasA", "MadeOf", "Synonym", "Antonym"]
+verb_rel = ["UsedFor", "CapableOf"]
+
+def get_conceptnet(word,rel):
+    obj = requests.get(f"https://api.conceptnet.io/c/en/{word}?limit=100/r/{rel}").json()
+    return obj
+
+def get_nouns(word):
+    noun_set = []
+    obj = get_conceptnet(word)
+    for edge in obj['edges']:
+        # skip non-English edges
+        if 'language' in edge['end'] and edge['end']['language'] != 'en':
+            continue
+        if edge['rel']['label'] in noun_rel:
+            noun_set.append(edge['end']['label'])
+    return list(set(noun_set))
+
+def get_verbs(word):
+    verb_set = []
+    obj = get_conceptnet(word)
+    for edge in obj['edges']:
+        # skip non-English edges
+        if 'language' in edge['end'] and edge['end']['language'] != 'en':
+            continue
+        #print(edge['rel']['label'])
+        if edge['rel']['label'] in verb_rel:
+            verb_set.append(edge['end']['label'])
+    return list(set(verb_set))
+
+#print(get_verbs("book"))
+#print(get_nouns("book"))
+
+
+get_conceptnet("book","UsedFor")
